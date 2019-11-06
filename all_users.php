2b488d88f10121ec0cffe5dc6f1c46e12918fc04
Ekkaim <loic.rieudebat@iut-rodez.fr>
2019-11-06 12:13:44 +0100

    Fin du premier point de l'activité 3

--- all_users.php
+++ all_users.php
@@ -26,14 +26,14 @@
 			}catch(PDOException$e){
 				throw new PDOException($e->getMessage(),(int)$e->getCode());
 			}
-			$stmt = $pdo->query('SELECT * FROM users ORDER BY username');
+			$stmt = $pdo->query('SELECT * FROM users JOIN status ON users.status_id=status.id WHERE status.name="Active account" ORDER BY username');
 			echo "<table border=\"1px\">";
 			while($row = $stmt->fetch()){
 				echo "<tr>";
 				echo "<td>".$row['id']."</td>";
 				echo "<td>".$row['username']."</td>";
 				echo "<td>".$row['email']."</td>";
-				echo "<td>".$row['status_id']."</td>";
+				echo "<td>".$row['name']."</td>";
 				echo "</tr>";
 			}
 			echo "</table>";
