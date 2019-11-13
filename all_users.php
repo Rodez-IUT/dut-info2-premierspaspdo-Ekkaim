8c5dc3dcc8fc391af86e1f3d758df67da838aeba
Ekkaim <loic.rieudebat@iut-rodez.fr>
2019-11-13 11:17:53 +0100

    fix #3.1 Filtre sur status et username

--- all_users.php
+++ all_users.php
@@ -11,6 +11,9 @@
 	</head>
 	<body>
 		<?php 
+			$status='Active account';
+			$lettre='e';
+			
 			$host='localhost';
 			$db='my_activities';
 			$user='root';
@@ -26,17 +29,29 @@
 			}catch(PDOException$e){
 				throw new PDOException($e->getMessage(),(int)$e->getCode());
 			}
-			$stmt = $pdo->query('SELECT * FROM users JOIN status ON users.status_id=status.id WHERE status.name="Active account" ORDER BY username');
-			echo "<table border=\"1px\">";
-			while($row = $stmt->fetch()){
-				echo "<tr>";
-				echo "<td>".$row['id']."</td>";
-				echo "<td>".$row['username']."</td>";
-				echo "<td>".$row['email']."</td>";
-				echo "<td>".$row['name']."</td>";
-				echo "</tr>";
-			}
-			echo "</table>";
+			$stmt = $pdo->query('SELECT users.id, users.username, users.email, status.name 
+								FROM users 
+								JOIN status 
+								ON users.status_id=status.id 
+								WHERE status.name="'.$status.'" 
+								AND users.username LIKE "e%"
+								ORDER BY username');
+			echo "<div class=\"container\">";
+				echo "<div class=\"row\">";
+					echo "<div class=\"col-lg-1\"><strong> ID </strong></div>";
+					echo "<div class=\"col-lg-3\"><strong> Username </strong></div>";
+					echo "<div class=\"col-lg-5\"><strong> Email </strong></div>";
+					echo "<div class=\"col-lg-3\"><strong> Status </strong></div>";
+				echo "</div>";
+				while($row = $stmt->fetch()){
+					echo "<div class=\"row\">";
+						echo "<div class=\"col-lg-1\">".$row['id']."</div>";
+						echo "<div class=\"col-lg-3\">".$row['username']."</div>";
+						echo "<div class=\"col-lg-5\">".$row['email']."</div>";
+						echo "<div class=\"col-lg-3\">".$row['name']."</div>";
+					echo "</div>";
+				}
+			echo "</div>";
 		?>
 	</body>
 </html>
