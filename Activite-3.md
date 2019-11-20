1609799eeec5272f91fd8b306b370252281049a7
Ekkaim <loic.rieudebat@iut-rodez.fr>
2019-11-20 11:28:37 +0100

    fix #3.3 Activité 3 terminée

---
+++ Activite-3.md
@@ -0,0 +1,2 @@
+Ce la nous affiche tout le contenu de la base de donnée car la suite de caractere "a%' or 1 = 1 ; --"  renvoit
+les lignes de la table "users", car OR 1 = 1 est toujours VRAI. Puis ce met le Order By en commentaire.
--- all_users.php
+++ all_users.php
@@ -10,9 +10,9 @@
 		<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
 	</head>
 	<body>
-		<form method="post">
+		<form method="get">
 			<label>Start with letter : </label> 
-			<input type="text" id="letter" name="letter" required maxlength="1" size="10">
+			<input type="text" id="letter" name="letter" required maxlength="20" size="10">
 			<label> and status is : </label> 	
 			<select name="status">
 				<option value="Active account" selected>Active account</option>
@@ -22,8 +22,13 @@
 		</form>
 				
 		<?php 
-			$status= $_POST['status'];
-			$lettre= $_POST['letter'];
+			if (isset($_GET['status'],$_GET['letter'])) {
+				$status= $_GET['status'];
+				$lettre= $_GET['letter'];
+			} else {
+				$status=0;
+				$lettre=0;
+			}
 			
 			$host='localhost';
 			$db='my_activities';
@@ -39,42 +44,30 @@
 				$pdo=new PDO($dsn,$user,$pass,$options);
 			}catch(PDOException$e){
 				throw new PDOException($e->getMessage(),(int)$e->getCode());
-			}
-			if (isset($lettre, $status)) {
-				$stmt = $pdo->query('SELECT users.id, users.username, users.email, status.name 
-									FROM users 
-									JOIN status 
-									ON users.status_id=status.id 
-									WHERE status.name="'.$status.'" 
-									AND users.username LIKE "'.$lettre.'%"
-									ORDER BY username');
+			}				
+			$stmt = $pdo->query("SELECT users.id, users.username, users.email, status.name 
+								FROM users 
+								JOIN status 
+								ON users.status_id=status.id 
+								WHERE status.name='$status' 
+								AND users.username LIKE '$lettre%' ORDER BY username");
 								
-				echo "<div class=\"container\">";
-					echo "<div class=\"row\">";
-						echo "<div class=\"col-lg-1\"><strong> ID </strong></div>";
-						echo "<div class=\"col-lg-3\"><strong> Username </strong></div>";
-						echo "<div class=\"col-lg-5\"><strong> Email </strong></div>";
-						echo "<div class=\"col-lg-3\"><strong> Status </strong></div>";
-					echo "</div>";
-					while($row = $stmt->fetch()){
-						echo "<div class=\"row\">";
-							echo "<div class=\"col-lg-1\">".$row['id']."</div>";
-							echo "<div class=\"col-lg-3\">".$row['username']."</div>";
-							echo "<div class=\"col-lg-5\">".$row['email']."</div>";
-							echo "<div class=\"col-lg-3\">".$row['name']."</div>";
-						echo "</div>";
-					}
+			echo "<div class=\"container\">";
+				echo "<div class=\"row\">";
+					echo "<div class=\"col-lg-1\"><strong> ID </strong></div>";
+					echo "<div class=\"col-lg-3\"><strong> Username </strong></div>";
+					echo "<div class=\"col-lg-5\"><strong> Email </strong></div>";
+					echo "<div class=\"col-lg-3\"><strong> Status </strong></div>";
 				echo "</div>";
-			} else {
-				echo "<div class=\"container\">";
+				while($row = $stmt->fetch()){
 					echo "<div class=\"row\">";
-						echo "<div class=\"col-lg-1\"><strong> ID </strong></div>";
-						echo "<div class=\"col-lg-3\"><strong> Username </strong></div>";
-						echo "<div class=\"col-lg-5\"><strong> Email </strong></div>";
-						echo "<div class=\"col-lg-3\"><strong> Status </strong></div>";
-					echo "</div>";
-				echo "</div>";
-			}
+						echo "<div class=\"col-lg-1\">".$row['id']."</div>";
+						echo "<div class=\"col-lg-3\">".$row['username']."</div>";
+						echo "<div class=\"col-lg-5\">".$row['email']."</div>";
+					echo "<div class=\"col-lg-3\">".$row['name']."</div>";
+					echo "</div>"
+				}
+			echo "</div>";
 		?>
 	</body>
 </html>
